31cca6fe7d1552f9c1fbfa6ffae4be4c50446c22
Tom Williams <tomwilliams@Thomass-Macintosh.local>
2016-06-14 00:36:13 +0100

    sequencer and oscillator

--- src/index.js
+++ src/index.js
@@ -1,30 +1,117 @@
-var context = new AudioContext();
-var sources = [];
+const context = new AudioContext();
 
-var promises = [
-	fetch('http://localhost:8000/kick.wav'),
-	fetch('http://localhost:8000/hat.wav'),
-	fetch('http://localhost:8000/snare.aiff')
-];
+///\\\ *** SEQUENCER *** \\\///
 
-var setup = new Promise(function(resolve) {
-	Promise.all(promises)
-	.then(function(resolved) {
-		resolved.forEach(function(res) {
-			var source = context.createBufferSource();
-			return res.arrayBuffer().then(function(buffer) {
-				console.log('i')
-				return context.decodeAudioData(buffer).then(function(buffer) { // update chrome
-					sources.push(source);
-					source.buffer = buffer;
-					source.connect(context.destination);
-				})
-			})
+const buffers = {};
+
+const samples = {
+	kick: 'http://localhost:8000/kick.wav',
+	hat: 'http://localhost:8000/hat.wav',
+	snare: 'http://localhost:8000/snare.wav'
+};
+
+// fetch samples
+const samplePromises = [ fetch(samples.kick), fetch(samples.hat), fetch(samples.snare) ];
+
+Promise.all(samplePromises)
+.then((sampleResponses) => {
+	sampleResponses.forEach((response, i) => {
+		response.arrayBuffer().then((arrayBuffer) => {
+			context.decodeAudioData(arrayBuffer, (audioBuffer) => {
+				buffers[Object.keys(samples)[i]] = audioBuffer;
+			});
 		});
-		resolve();
 	});
 });
 
-setup.then(function() {
-	console.log(sources)
+// create source in audio context and add audio buffer to it
+function playSample(sample, beat) {
+	var time = beat * 0.46; // about 130 bpm 
+	var source = context.createBufferSource();
+	source.buffer = buffers[sample];
+	source.connect(context.destination);
+	source.start(context.currentTime + time);
+}
+
+function playPhrase(firstBeat, noteObject) {
+	for (sample in noteObject) {
+		noteObject[sample].forEach((beat) => {
+			playSample(sample, beat + firstBeat);
+		});	
+	}
+}
+
+// sequences
+const intro = { kick: [ 0, 1, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 3.75, 4, 5, 6, 7 ] };
+
+const verse = {
+	kick: [ 0, 1, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 3.75, 4, 5, 6, 7 ],
+	hat: [ 0.5, 0.75, 1.5, 1.75, 2.5, 2.75, 3.5, 3.75, 4.5, 4.75, 5.5, 5.75, 6.5, 6.75, 7.5, 7.75 ],
+	snare: [ 1, 3, 5, 7 ]
+};
+
+function playIntro(firstBeat) {
+	playPhrase(firstBeat, intro);
+}
+
+function playVerse(firstBeat) {
+	playPhrase(firstBeat, verse)
+
+	if (firstBeat < 200) {
+		playVerse(firstBeat + 8, verse);	
+	}
+}
+
+///\\\ *** SYNTHESIZER *** \\\///
+
+// create a filter and a gain node
+const filterNode = context.createBiquadFilter();
+filterNode.type = 'lowpass';
+filterNode.frequency.value = 10000;
+filterNode.connect(context.destination);
+
+const filterVal = document.getElementById('filterVal');
+
+filterVal.addEventListener('input', function(event) {
+	filterNode.frequency.value = event.target.value;
 });
+
+const gainNode = context.createGain();
+gainNode.gain.value = 0.5;
+gainNode.connect(filterNode);
+
+// play oscillator of given frequency
+function playOscillator(frequency) {
+	var oscillator = context.createOscillator();
+	oscillator.type = 'square';
+	oscillator.frequency.value = frequency;
+	oscillator.connect(gainNode);
+	oscillator.start(context.currentTime);
+	oscillator.stop(context.currentTime + 0.25);
+}
+
+///\\\ *** INTERACTION *** \\\///
+
+window.onkeydown = function(event) {
+	switch(event.keyCode) {
+	    case 65:
+	    	playIntro(0);
+	    	playIntro(8);
+	        playVerse(16);
+	        break;
+	    case 72:
+	        playOscillator(65.41);
+	        break;
+	    case 74:
+	        playOscillator(73.42);
+	        break;
+	    case 75:
+	        playOscillator(87.31);
+	        break;
+	    case 76:
+	        playOscillator(98.00);
+	        break;
+	    default:
+	        break;
+	}
+};
